a8ed8d807cba3447333442202c6d084bf4d96040
lockjio <jio.buenviaje@gmail.com>
2020-01-27 13:46:15 -0500

    Speed up gotop function

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,7 @@ export default withRedux(initStore)(
 					<Provider store={store}>
 						<Component {...pageProps} />
 					</Provider>
-					<GoTop scrollStepInPx="50" delayInMs="16.66" />
+					<GoTop scrollStepInPx="50" />
 				</Container>
 			);
 		}
