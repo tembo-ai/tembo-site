92244fd6eda1e8c1f69773a952075ad05703ebad
lockjio <jio.buenviaje@gmail.com>
2020-01-27 13:36:03 -0500

    Fix 404 error page

--- components/Error/Banner.js
+++ components/Error/Banner.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 
 class Banner extends Component {
 	render() {
 		return (
 			<section className="page-title-banner">
+				<Particles
+					params={{
+						particles: {
+							number: {
+								value: 25
+							},
+							size: {
+								value: 3
+							}
+						},
+						interactivity: {
+							events: {
+								onhover: {
+									enable: true,
+									mode: "repulse"
+								},
+								onclick: {
+									enable: true,
+									mode: "push"
+								}
+							}
+						}
+					}}
+					style={{
+						width: "100%",
+						backgroundColor: "#3e0f5e",
+						position: "absolute",
+						top: "0",
+						left: "0"
+					}}
+				/>
 				<div className="container">
 					<div className="row">
 						<div className="col-lg-12">
--- pages/_error.js
+++ pages/_error.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import Banner from '../components/Error/Banner';
+import React, { Component } from "react";
+import Link from "next/link";
+import Banner from "../components/Error/Banner";
 
 class Error extends Component {
+	static getInitialProps({ res, err }) {
+		const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+		return { statusCode };
+	}
 
-    static getInitialProps({ res, err }) {
-        const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-        return { statusCode };
-    }
+	render() {
+		return (
+			<React.Fragment>
+				<Banner />
 
-    render() {
-        return (
-            <React.Fragment>
-                <Banner />
-
-                <section className="error-area ptb-100">
-                    <div className="container">
-                        <div className="notfound">
-                            <div className="notfound-404"></div>
-                            <h2>{this.props.statusCode}</h2>
-                            <span>Oops!! Page not be Found</span>
-                            <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-                            <Link href="#">
-                                <a className="btn btn-primary">Back To Homepage</a>
-                            </Link>
-                        </div>
-                    </div>
-                </section>
-            </React.Fragment>
-        );
-    }
+				<section className="error-area ptb-100">
+					<div className="container">
+						<div className="notfound">
+							<div className="notfound-404"></div>
+							<h2>{this.props.statusCode}</h2>
+							<span>
+								Oh no! The page you are looking for cannot be
+								found.
+							</span>
+							<p>
+								Sorry, but the page you are looking for does not
+								exist or is temporarily unavailable. We
+								apologize for the inconvenience.
+							</p>
+							<Link href="/">
+								<a className="btn btn-primary">
+									Back To Homepage
+								</a>
+							</Link>
+						</div>
+					</div>
+				</section>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Error;
