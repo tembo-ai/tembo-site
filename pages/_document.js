43022918fff20a5333dbb1f6d268edfcd1256af2
lockjio <jio.buenviaje@gmail.com>
2020-01-28 04:21:55 -0500

    Fix html head errors

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,8 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<meta charset="utf-8" />
-					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+					<meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta
 						name="viewport"
 						content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -24,7 +24,6 @@ class MyDocument extends Document {
 					<meta name="author" content="Jio Buenviaje" />
 
 					{/* Set the page title */}
-					<title>Tembo AI</title>
 					<meta property="og:title" content="Tembo AI" />
 					<meta property="twitter:title" content="Tembo AI" />
 
