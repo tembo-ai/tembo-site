48e06955e2d615588e4aba86d5c7d2c820025f2b
lockjio <jio.buenviaje@gmail.com>
2020-01-28 04:22:14 -0500

    Fix contact form using axios and Formspree

--- components/Contents/Contact.js
+++ components/Contents/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Form from "./Form";
 
 class Contact extends Component {
 	render() {
@@ -19,87 +20,7 @@ class Contact extends Component {
 						</div>
 
 						<div className="col-lg-6 col-md-12">
-							<form id="contactForm">
-								<div className="row">
-									<div className="col-lg-6 col-md-12">
-										<div className="form-group">
-											<input
-												type="text"
-												className="form-control"
-												required={true}
-												data-error="First Name"
-												placeholder="First Name"
-											/>
-											<div className="help-block with-errors"></div>
-										</div>
-									</div>
-									<div className="col-lg-6 col-md-12">
-										<div className="form-group">
-											<input
-												type="text"
-												className="form-control"
-												required={true}
-												data-error="Last Name"
-												placeholder="Last Name"
-											/>
-											<div className="help-block with-errors"></div>
-										</div>
-									</div>
-
-									<div className="col-lg-12 col-md-12">
-										<div className="form-group">
-											<input
-												type="email"
-												className="form-control"
-												required={true}
-												data-error="Please enter your email"
-												placeholder="Email"
-											/>
-											<div className="help-block with-errors"></div>
-										</div>
-									</div>
-
-									<div className="col-lg-12 col-md-6">
-										<div className="form-group">
-											<input
-												type="text"
-												className="form-control"
-												placeholder="Subject"
-											/>
-										</div>
-									</div>
-
-									<div className="col-lg-12 col-md-12">
-										<div className="form-group">
-											<textarea
-												name="message"
-												className="form-control"
-												id="message"
-												cols="30"
-												rows="5"
-												required
-												data-error="Write your message"
-												placeholder="Your Message"
-											/>
-											<div className="help-block with-errors"></div>
-										</div>
-									</div>
-
-									<div className="col-lg-12 col-md-12">
-										<button
-											type="submit"
-											className="btn btn-primary"
-										>
-											Send Message
-										</button>
-										<div
-											id="msgSubmit"
-											className="h3 text-center hidden"
-										></div>
-										<div className="clearfix"></div>
-									</div>
-								</div>
-							</form>
+							<Form />
 						</div>
 					</div>
 				</div>
---
+++ components/Contents/Form.js
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default () => {
+	const [status, setStatus] = useState({
+		submitted: false,
+		submitting: false,
+		info: { error: false, msg: null }
+	});
+	const [inputs, setInputs] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		subject: "",
+		message: ""
+	});
+	const handleServerResponse = (ok, msg) => {
+		if (ok) {
+			setStatus({
+				submitted: true,
+				submitting: false,
+				info: { error: false, msg: msg }
+			});
+			setInputs({
+				firstName: "",
+				lastName: "",
+				email: "",
+				subject: "",
+				message: ""
+			});
+		} else {
+			setStatus({
+				info: { error: true, msg: msg }
+			});
+		}
+	};
+	const handleOnChange = e => {
+		e.persist();
+		setInputs(prev => ({
+			...prev,
+			[e.target.id]: e.target.value
+		}));
+		setStatus({
+			submitted: false,
+			submitting: false,
+			info: { error: false, msg: null }
+		});
+	};
+	const handleOnSubmit = e => {
+		e.preventDefault();
+		setStatus(prevStatus => ({ ...prevStatus, submitting: true }));
+		axios({
+			method: "POST",
+			url: "https://formspree.io/myywgdng",
+			data: inputs
+		})
+			.then(response => {
+				handleServerResponse(
+					true,
+					"Thank you, your message has been submitted."
+				);
+			})
+			.catch(error => {
+				handleServerResponse(false, error.response.data.error);
+			});
+	};
+	return (
+		<main>
+			<form id="contactForm" onSubmit={handleOnSubmit}>
+				<div className="row">
+					<div className="col-lg-6 col-md-12">
+						<div className="form-group">
+							<input
+								id="firstName"
+								type="text"
+								name="firstName"
+								onChange={handleOnChange}
+								required
+								value={inputs.firstName}
+								className="form-control"
+								data-error="Please enter your name"
+								placeholder="First Name"
+							/>
+							<div className="help-block with-errors"></div>
+						</div>
+					</div>
+					<div className="col-lg-6 col-md-12">
+						<div className="form-group">
+							<input
+								id="lastName"
+								type="text"
+								name="lastName"
+								onChange={handleOnChange}
+								required
+								value={inputs.lastName}
+								className="form-control"
+								data-error="Please enter your name"
+								placeholder="Last Name"
+							/>
+							<div className="help-block with-errors"></div>
+						</div>
+					</div>
+
+					<div className="col-lg-12 col-md-12">
+						<div className="form-group">
+							<input
+								id="email"
+								type="email"
+								name="_replyto"
+								onChange={handleOnChange}
+								required
+								value={inputs.email}
+								className="form-control"
+								data-error="Please enter your email"
+								placeholder="Email"
+							/>
+							<div className="help-block with-errors"></div>
+						</div>
+					</div>
+
+					<div className="col-lg-12 col-md-6">
+						<div className="form-group">
+							<input
+								id="subject"
+								type="text"
+								name="subject"
+								onChange={handleOnChange}
+								required
+								value={inputs.subject}
+								className="form-control"
+								data-error="Please enter a subject"
+								placeholder="Subject"
+							/>
+						</div>
+					</div>
+
+					<div className="col-lg-12 col-md-12">
+						<div className="form-group">
+							<textarea
+								id="message"
+								type="text"
+								name="message"
+								onChange={handleOnChange}
+								required
+								value={inputs.message}
+								className="form-control"
+								cols="30"
+								rows="5"
+								data-error="Please write your message"
+								placeholder="Message"
+							/>
+							<div className="help-block with-errors"></div>
+						</div>
+					</div>
+
+					<div className="col-lg-12 col-md-12">
+						<button
+							type="submit"
+							disabled={status.submitting}
+							className="btn btn-primary"
+						>
+							{!status.submitting
+								? !status.submitted
+									? "Submit"
+									: "Submitted"
+								: "Submitting..."}
+						</button>
+						{status.info.error && (
+							<>
+								<div
+									id="msgSubmit"
+									className="h4 text-center hidden"
+								>
+									Error, your message has not been sent!
+								</div>
+								<div className="clearfix"></div>
+							</>
+						)}
+						{!status.info.error && status.info.msg && (
+							<>
+								<div
+									id="msgSubmit"
+									className="h4 text-center hidden"
+								>
+									Success, we have received your message!
+								</div>
+								<div className="clearfix"></div>
+							</>
+						)}
+					</div>
+				</div>
+			</form>
+		</main>
+	);
+};
