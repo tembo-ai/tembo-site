201825f822f8467c2ed6e4ebfa65ebd49926f136
lockjio <jio.buenviaje@gmail.com>
2020-02-07 11:13:26 -0500

    Make particles.js whole bg for 404 page

--- pages/_error.js
+++ pages/_error.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Link from "next/link";
-import Banner from "../components/Contents/ErrorBanner";
+import Particles from "react-particles-js";
 
 class Error extends Component {
 	static getInitialProps({ res, err }) {
@@ -11,27 +11,58 @@ class Error extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Banner />
-
 				<section className="error-area ptb-100">
 					<div className="container">
-						<div className="notfound">
-							<div className="notfound-404"></div>
-							<h2>{this.props.statusCode}</h2>
-							<span>
-								Oh no! The page you are looking for cannot be
-								found.
-							</span>
-							<p>
-								Sorry, but the page you are looking for does not
-								exist or is temporarily unavailable. We
-								apologize for the inconvenience.
-							</p>
-							<Link href="/">
-								<a className="btn btn-primary">
-									Back To Homepage
-								</a>
-							</Link>
+						<Particles
+							params={{
+								particles: {
+									number: {
+										value: 25
+									},
+									size: {
+										value: 3
+									}
+								},
+								interactivity: {
+									events: {
+										onhover: {
+											enable: true,
+											mode: "repulse"
+										},
+										onclick: {
+											enable: true,
+											mode: "push"
+										}
+									}
+								}
+							}}
+							style={{
+								width: "100%",
+								backgroundColor: "#3e0f5e",
+								position: "absolute",
+								top: "0",
+								left: "0"
+							}}
+						/>
+						<div className="container">
+							<div className="notfound">
+								<div className="notfound-404"></div>
+								<h2>{this.props.statusCode}</h2>
+								<span>
+									Oh no! The page you are looking for cannot
+									be found.
+								</span>
+								<p>
+									Sorry, but the page you are looking for does
+									not exist or is temporarily unavailable. We
+									apologize for the inconvenience.
+								</p>
+								<Link href="/">
+									<a className="btn btn-primary">
+										Back To Homepage
+									</a>
+								</Link>
+							</div>
 						</div>
 					</div>
 				</section>
